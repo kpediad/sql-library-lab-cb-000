71475a2f77eecec3872166be47e57fd495ea93af
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-10-10 15:53:34 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series JOIN books ON series.id = books.series_id JOIN characters ON books.character_id = characters.id WHERE characters.species = 'human' GROUP BY series.title ORDER BY COUNT(series.title) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
